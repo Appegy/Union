078f71dd815571810dbeaf7305e91a4e406afbc3
Ivan Murashka <imurashka.me@gmail.com>
2025-02-02 00:16:34 +0100

    clean generation of indexeers and properties

--- Appegy.Union.Generator~/Appegy.Union.Generator/Expose/ExposeAttributeGenerator.cs
+++ Appegy.Union.Generator~/Appegy.Union.Generator/Expose/ExposeAttributeGenerator.cs
@@ -22,7 +22,8 @@ public class ExposeAttributeGenerator : IIncrementalGenerator
             new ExposeDeclarationPart(),
             new ScopedPart<ExposeAttributePartInput>([
                 new ExposeInterfacePart([
-                    new ExposePropertiesPart()
+                    new ImplementProperties(),
+                    new ImplementIndexers()
                 ]),
             ])
         ])
