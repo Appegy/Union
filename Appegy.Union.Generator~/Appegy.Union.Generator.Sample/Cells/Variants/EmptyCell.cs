0c51177ff5e1635e6dab39d488fbfb7ee5ca8962
Ivan Murashka <imurashka.me@gmail.com>
2025-03-07 19:33:03 +0100

    setup sample app for debugging

--- Appegy.Union.Generator~/Appegy.Union.Generator.Sample/Cells/Variants/EmptyCell.cs
+++ Appegy.Union.Generator~/Appegy.Union.Generator.Sample/Cells/Variants/EmptyCell.cs
@@ -1,11 +1,26 @@
-﻿using Appegy.Union.Generator.Sample.Cells.Interfaces;
+﻿using System;
 
-namespace Appegy.Union.Generator.Sample.Cells.Variants
+namespace Appegy.Union.Generator.Sample.Cells
 {
-    public struct EmptyCell : IPuzzleCell, IMovableCell, IMatchableCell
+    public struct EmptyCell : IPuzzleCell, IMovableCell, IMatchableCell, IEquatable<EmptyCell>
     {
         public short Id => -1;
         public bool Movable => false;
         public bool Matchable => false;
+
+        public bool Equals(EmptyCell other)
+        {
+            return true;
+        }
+
+        public override bool Equals(object? boxed)
+        {
+            return boxed is EmptyCell other && Equals(other);
+        }
+
+        public override int GetHashCode()
+        {
+            return Id;
+        }
     }
 }
