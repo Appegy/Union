fc0fd0f52ca8845b8a08586b5e96f6b5419bd67a
Ivan Murashka <imurashka.me@gmail.com>
2025-03-08 19:35:29 +0100

    add Shapes project for tests and diagnostic

---
+++ Appegy.Union.Generator~/Appegy.Union.Generator.Shapes/Shapes/Rectangle.cs
@@ -0,0 +1,38 @@
+﻿namespace Appegy.Union.Generator.Shapes;
+
+public readonly struct Rectangle(double width, double height) : IShape, IEquatable<Rectangle>
+{
+    public double Width { get; } = width;
+    public double Height { get; } = height;
+    public double Area => Width * Height;
+
+    public override string ToString()
+    {
+        return nameof(Rectangle);
+    }
+
+    public bool Equals(Rectangle other)
+    {
+        return Width.Equals(other.Width) && Height.Equals(other.Height);
+    }
+
+    public override bool Equals(object? boxed)
+    {
+        return boxed is Rectangle other && Equals(other);
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Width, Height);
+    }
+
+    public static bool operator ==(Rectangle left, Rectangle right)
+    {
+        return left.Equals(right);
+    }
+
+    public static bool operator !=(Rectangle left, Rectangle right)
+    {
+        return !(left == right);
+    }
+}
