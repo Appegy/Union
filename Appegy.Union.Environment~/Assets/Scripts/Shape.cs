092c1cc95ce454dffff47014a7efc71306343c5c
Ivan Murashka <imurashka.me@gmail.com>
2025-04-15 22:32:52 +0200

    update unity sample to use generated attributes

---
+++ Appegy.Union.Environment~/Assets/Scripts/Shape.cs
@@ -0,0 +1,15 @@
+﻿using Appegy.Union.Sample.Variants;
+
+namespace Appegy.Union.Sample
+{
+    public interface IShape
+    {
+        double Area { get; }
+    }
+
+    [Union(typeof(Circle), typeof(Rectangle), typeof(Hexagon))]
+    [Expose(typeof(IShape))]
+    public partial struct Shape
+    {
+    }
+}
---
+++ Appegy.Union.Environment~/Assets/Scripts/Variants/Hexagon.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Appegy.Union.Sample.Variants
+{
+    public readonly struct Hexagon : IShape, IEquatable<Hexagon>
+    {
+        public double SideLength { get; }
+
+        public double Area => 3 * Math.Sqrt(3) * SideLength * SideLength / 2;
+
+        public Hexagon(double sideLength)
+        {
+            SideLength = sideLength;
+        }
+
+        public override string ToString()
+        {
+            return nameof(Hexagon);
+        }
+
+        public bool Equals(Hexagon other)
+        {
+            return SideLength.Equals(other.SideLength);
+        }
+
+        public override bool Equals(object boxed)
+        {
+            return boxed is Hexagon other && Equals(other);
+        }
+
+        public override int GetHashCode()
+        {
+            return SideLength.GetHashCode();
+        }
+
+        public static bool operator ==(Hexagon left, Hexagon right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(Hexagon left, Hexagon right)
+        {
+            return !(left == right);
+        }
+    }
+}
