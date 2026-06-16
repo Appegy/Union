a2815c7f0f8a4bd5711ee135833559cd39b4ffc1
Ivan Murashka <imurashka.me@gmail.com>
2026-06-16 19:07:21 +0200

    Release tooling: signed release + bump-version (bump to 1.0.3) (#29)

---
+++ src/Runtime/PackageInfo.cs
@@ -0,0 +1,8 @@
+namespace Appegy.Union
+{
+    public static class PackageInfo
+    {
+        public const string Name = "com.appegy.union";
+        public const string Version = "1.0.3";
+    }
+}
